14cf42674d0c447aa8cd18c3322e7db639ae3435
Tomáš Kolečkář <tomanak159@seznam.cz>
2021-03-22 17:18:55 +0100

    skoro dokonceny chat podle tutorialu

--- server.js
+++ server.js
@@ -2,22 +2,35 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const socketio = require("socket.io");
-
+const formatMessage = require("./utils/messages");
+const {userJoin, getCurrentUser,userLeave,getRoomUsers} = require("./utils/users");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const botName = "Admin";
 
 const PORT = 8080 || procces.env.PORT;
 
 io.on("connection", socket => {
-    socket.emit("message", "Welcome to Chad!");
+    socket.on("joinRoom", ({username, room})=>{
+        const user = userJoin(socket.id, username, room);
 
-    socket.broadcast.emit("message","A user has joined Chad !");
+        socket.join(user.room);
+
+        socket.emit("message", formatMessage(botName, "Welcome to Chad !"));
+
+        socket.broadcast.to(user.room).emit("message", formatMessage(botName, `${user.username} has joined the chat`));
+    })
+    
     socket.on("disconnect", () => {
-        io.emit("message", "A user has left the Chad");
+        const user = userLeave(socket.id);
+        if(user){
+            io.to(user.room).emit("message", formatMessage(botName, `${user.username} has disconnected`)); 
+        }
     });
     socket.on("chatMessage",(msg)=> {
-        io.emit("message",msg);
+        const user = getCurrentUser(socket.id);
+        io.to(user.room).emit("message",formatMessage(user.username,msg));
     });
 });
 app.use(express.static(path.join(__dirname,"public")));
